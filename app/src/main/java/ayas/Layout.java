4581d3103e7bda90845fdfa6f1bcdb8faf24b554
AppersOfficial <122030777+AppersOfficial@users.noreply.github.com>
2023-01-31 20:38:11 -0300

    Update Layout.java

--- app/src/main/java/ayas/Layout.java
+++ app/src/main/java/ayas/Layout.java
@@ -6,17 +6,28 @@ import java.util.*;
 public class Layout {
 
     private String id;
+    public static final int VIEW_GONE = 8;
+    public static final int CENTER = 17;
+    public static final int CENTER_HORIZONTAL = 1;
+    public static final int CENTER_VERTICAL = 16;
+    public static final int LEFT = 3;
+    public static final int RIGHT = 5;
+    public static final int BOTTOM = 80;
+    public static final int TOP = 48;
+    public static final int HORIZONTAL = 0;
+    public static final int VERTICAL = 1;
     public static final int MATCH = -1;
-    public static final int WRAP = 0;
-    public static final int VERTICAL = -1;
-    public static final int HORIZONTAL = -1;
-    public static final int CENTER = -1;
-    public static final int CENTER_HORIZONTAL = -1;
-    public static final int CENTER_VERTICAL = -1;
-    public static final int TOP = -1;
-    public static final int BOTTOM = -1;
-    public static final int LEFT = -1;
-    public static final int RIGHT = -1;
+    public static final int WRAP = -2;
+    public static final int WHITE = 0xFFFFFF;
+    public static final int BLACK = 0x000000;
+    public static final int BLUE = 0x0000FF;
+    public static final int RED = 0xFF0000;
+    public static final int GREEN = 0x00FF00;
+    public static final int YELLOW = 0xFFFF00;
+    public static final int GREY = 0xCDCDCD;
+    public static final int BROWN = 0xFFFFFF;
+    public static final int PURPLE = 0xFFFFFF;
+    public static final int TRANSPARENT = 0x00000000;
 
     public Layout(){}
     
