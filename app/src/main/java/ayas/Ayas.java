d11c4adaba2741842391dab642773c1fc1eff775
AppersOfficial <122030777+AppersOfficial@users.noreply.github.com>
2023-01-31 20:16:51 -0300

    Update Ayas.java

--- app/src/main/java/ayas/Ayas.java
+++ app/src/main/java/ayas/Ayas.java
@@ -1,8 +1,24 @@
 package ayas;
 
+import android.content.Context;
 import androidx.fragment.app.FragmentActivity;
 import java.util.*;
 
 final public class Ayas extends FragmentActivity {
+    private Context context;
+    private ArrayList<String> ids = new ArrayList<>();
+
+    public Ayas(){}
     
+    public Ayas(Context context) {
+        this.context = context;
+    }
+
+    public void initPage(Context context) {
+        this.context = context;
+    }
+
+    public Context page() {
+        return context;
+    }
 }
