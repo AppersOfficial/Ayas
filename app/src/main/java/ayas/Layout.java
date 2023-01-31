20bcb127e8cfa09a07e76ad9ebccda7986e2a33b
AppersOfficial <122030777+AppersOfficial@users.noreply.github.com>
2023-01-31 20:35:20 -0300

    Update Layout.java

--- app/src/main/java/ayas/Layout.java
+++ app/src/main/java/ayas/Layout.java
@@ -1,9 +1,26 @@
 package ayas;
 
+import android.widget.LinearLayout;
+import java.util.*;
+
 public class Layout {
+
+    private String id;
+    public static final int MATCH = -1;
+    public static final int WRAP = 0;
+    public static final int VERTICAL = -1;
+    public static final int HORIZONTAL = -1;
+    public static final int CENTER = -1;
+    public static final int CENTER_HORIZONTAL = -1;
+    public static final int CENTER_VERTICAL = -1;
+    public static final int TOP = -1;
+    public static final int BOTTOM = -1;
+    public static final int LEFT = -1;
+    public static final int RIGHT = -1;
+
     public Layout(){}
     
-    public Layout() {
+    public Layout(String id, int width, int height, int orientation, int backgroundColor, int gravity) {
 
     }
 }
